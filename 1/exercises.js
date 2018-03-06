ccc2631bdf24b83f28cf9e07adbfd3523d761926
Thomas Elder <thomas.charles.elder@gmail.com>
2018-03-06 19:24:18 +1100

    Excercise 1

---
+++ 1/exercises.js
@@ -0,0 +1,7 @@
+/**
+ * Exercise 1: Write an arrow function that returns the string 'Hello World!'. 
+ */
+var helloWorld = () => "Hello World!"
+console.log(helloWorld())
+var helloWorld = () => { return "Hello World Again!" }
+console.log(helloWorld())
