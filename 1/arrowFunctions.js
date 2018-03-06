8ab8a0f06f1acb8d720dd898f27fb954492c4a94
Thomas Elder <thomas.charles.elder@gmail.com>
2018-03-06 17:22:03 +1100

    Created folder for chapter 1
    And started going through the code examples for fat arrow syntax

---
+++ 1/arrowFunctions.js
@@ -0,0 +1,8 @@
+// ES5
+var sum = function( a, b ) { return a + b; };
+
+// ES6
+var sum = ( a, b ) => { return a + b; };
+
+// ES6++ If the return value can be be described by one expression 
+var sum = (a, b) => a + b;
