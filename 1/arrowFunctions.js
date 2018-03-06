28fe574dedd4e58f798c84af4e3b66a1e90e04b1
Thomas Elder <thomas.charles.elder@gmail.com>
2018-03-06 17:26:24 +1100

    Added logging commands to arrowfunctions

--- 1/arrowFunctions.js
+++ 1/arrowFunctions.js
@@ -5,4 +5,11 @@ var sum = function( a, b ) { return a + b; };
 var sum = ( a, b ) => { return a + b; };
 
 // ES6++ If the return value can be be described by one expression 
-var sum = (a, b) => a + b;
+var sum = ( a, b ) => a + b;
+
+// ES6+++ If there is only one parameter, the parentheses are not needed there
+var square = a => a * a;
+
+console.log("Chapter 1 - Arrow Functions")
+console.log("The sum of 5 and 3 is ", sum(5, 3))
+console.log("The square of 4 is ", square(4))
