334129a77ca6bfd45c1fd6142755d1a4ec5f92d6
Thomas Elder <thomas.charles.elder@gmail.com>
2018-03-09 21:14:11 +1100

    Add eslint directive

---
+++ 5/deeperDestructuring.js
@@ -0,0 +1,31 @@
+/*eslint no-unused-vars: "off"*/
+
+/**
+ * Deeperdestructuring 
+ * 
+ * It is possible to destructure objects and arrays in any depth. 
+ * 
+ * Default values can also be used. 
+ * 
+ * Objects or arrays that don’t exist on the right become assigned to undefined on the left.
+ */
+
+let user = {
+  name: 'Ashley',
+  email: 'ashley@gmail.com',
+  lessonsSeen: [2, 5, 6, 7, 9],
+  nextLesson: 10
+};
+
+let {
+  lessonsSeen: [
+    first,
+    second,
+    third,
+    fourth,
+    fifth,
+    sixth = null,
+    seventh
+  ],
+  nextLesson: eighth
+} = user;
