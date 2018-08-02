ace8a48183f47fff9c155eee8f58379c13373172
Thomas Elder <thomas.charles.elder@gmail.com>
2018-08-02 20:17:57 +1000

    Chapter 9 - Avoiding name clashes

---
+++ 9/avoidingnameclashes.js
@@ -0,0 +1,9 @@
+
+/**
+ * When using symbols as identifiers for objects, we don’t have to set up a 
+ * global registry of available identifiers.
+ * 
+ * We also save creation of a new identifier, as all we need to do is create a Symbol().
+ * 
+ * Same holds for external libraries.
+ */
