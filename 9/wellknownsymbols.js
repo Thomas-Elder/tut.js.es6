c96df28317a83b7c62ea799663d8c39db60e8a5f
Thomas Elder <thomas.charles.elder@gmail.com>
2018-08-02 20:20:10 +1000

    Chapter 9 - Well known symbols

---
+++ 9/wellknownsymbols.js
@@ -0,0 +1,18 @@
+
+/**
+ * Wellknownsymbols
+ * There are some well known symbols6 defined to access and modify internal 
+ * JavaScript behavior. For example, you can redefine builtin methods, 
+ * operators, and loops. 
+ * 
+ * Redefining standard language behavior usually confuses other developers. 
+ * Ask yourself, is this skill going to move you forward in your career? 
+ * 
+ * We will not focus on well known symbols in this section. If there is a valid 
+ * use case for it, I will signal it in the corresponding lesson.
+ * 
+ * Otherwise, I suggest staying away from manipulating the expected behavior of 
+ * your code.
+ */
+
+/** Yesssir bossman. Necessary? */
