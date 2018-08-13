56cbb9dadbb19c82af74e4554bf6eed0cff240d5
Thomas Elder <thomas.charles.elder@gmail.com>
2018-08-13 14:21:35 +1000

    Chapter 11 - intro

---
+++ 11/stringsAndTemplateLiterals.js
@@ -0,0 +1,18 @@
+/**
+ * Strings and template literals
+ * 
+ * It is time to have a look at string manipulation and template literals. 
+ * 
+ * There has been a shift in the trends of web development in the last ten years that 
+ * moves the responsibility of rendering from the server side to the client side. 
+ * Therefore, the HTML code that you can see on screen is often assembled using 
+ * JavaScript. 
+ * 
+ * Furthermore, other trends such as isomorphic JavaScript have also emerged, where 
+ * full stack developers can use JavaScript to render templates both on the server 
+ * side and on the client side. This is why it is important for JavaScript developers 
+ * to learn about strings and template literals in depth. 
+ * 
+ * As a side benefit, template literals are more convenient to use than strings, 
+ * especially when it comes to string concatenation.
+ */
