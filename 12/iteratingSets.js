cd6a9f5fc912175eab7f07d42e37b8c08790b40f
Thomas Elder <thomas.charles.elder@gmail.com>
2019-02-22 11:48:58 +1100

    Chapter 12 stubbing out files

---
+++ 12/iteratingSets.js
@@ -0,0 +1,3 @@
+/**
+ * Iterating Sets
+ */
---
+++ 12/setsMapsAndTheirWeakVersions.js
@@ -0,0 +1,7 @@
+/**
+ * Chapter 12 - Sets,Maps,and their weak versions
+ * 
+ * This lesson introduces some data structures that we often use during coding: sets and maps.
+ * 
+ * Using these data structures prevent us from reinventing the wheel.
+ */
