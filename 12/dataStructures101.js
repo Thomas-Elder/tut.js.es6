d07ae16dcbeedae0cd982d478e531b5475397f58
Thomas Elder <thomas.charles.elder@gmail.com>
2019-02-22 13:29:45 +1100

    Chapter 12 Data Structures 101

---
+++ 12/dataStructures101.js
@@ -0,0 +1,17 @@
+/**
+ * Datastructures101
+ * 
+ * A set is an unordered collection of distinct elements. An element is said to be a member of the set, 
+ * if the set contains the element
+ * Adding an element to the set that’s already a member does not change the set. There are some operations
+ * defined on sets. These are: 
+ * union: e becomes a member of union( A, B ), if e is a member of A, or e is a member of B
+ * intersection: e becomes a member of intersection( A, B ),if e is a member of A, and e is a member of B
+ * difference: e becomes a member of difference( A, B ), if e is a member of A, and e is not a member of B
+ * 
+ * A map is a collection of key-value pairs. If you provide a key, you can retrieve the corresponding value.
+ * This sounds very much like a simple object.
+ * 
+ * However, there are major differences between objects and maps. Objects can only work as maps under very
+ * special circumstances.
+ */
